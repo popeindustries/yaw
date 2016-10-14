5c93a578feb43ac1932c712e7f4796f2bce7d7da
Alexander Pope <alex@pope-industries.com>
2016-10-14 13:12:17 +0200

    clean up

--- index.js
+++ index.js
@@ -1,139 +1,137 @@
-var fs = require('fs')
-	, path = require('path')
-	, util = require("util")
-	, EventEmitter = require('events').EventEmitter
-	, existsSync = fs.existsSync || path.existsSync
-	, RE_IGNORE = /^[.~]|~$/
-	, THROTTLE_TIMEOUT = 100;
+'use strict';
 
-module.exports = Watcher;
+const Emitter = require('events');
+const fs = require('fs');
+const path = require('path');
 
-/**
- * Constructor
- */
-function Watcher() {
-	EventEmitter.call(this);
+const RE_IGNORE = /^[.~]|~$/;
+const THROTTLE_TIMEOUT = 100;
 
-	this.watchers = {};
-	this._throttling = {
-		'create': 0,
-		'delete': 0,
-		'change': 0
-	};
+module.exports = function watcherFactory () {
+  return new Watcher();
+};
+
+class Watcher extends Emitter {
+  /**
+   * Constructor
+   */
+  constructor () {
+    super();
+
+    this.watchers = {};
+    this._throttling = {
+      create: 0,
+      delete: 0,
+      change: 0
+    };
+  }
+
+  /**
+   * Watch a 'source' file or directory for changes
+   * @param {String} source
+   */
+  watch (source) {
+    if (RE_IGNORE.test(path.basename(source))) return;
+
+    fs.stat(source, (err, stats) => {
+      let lastChange;
+
+      if (err) {
+        this.emit('error', err);
+      } else {
+        lastChange = stats.mtime.getTime();
+        // Recursively parse items in directory
+        if (stats.isDirectory()) {
+          fs.readdir(source, (err, files) => {
+            if (err) this.emit('error', err);
+            files.forEach((file) => {
+              this.watch(path.resolve(source, file));
+            });
+          });
+        }
+      }
+
+      // Store watcher objects
+      this.watchers[source] = fs.watch(source, (evt, filename) => {
+        // Deleted
+        if (!fs.existsSync(source)) {
+          this.unwatch(source);
+          this._throttleEvent('delete', source);
+          return;
+        }
+
+        fs.stat(source, (err, stats) => {
+          if (err) return this.emit('error', err);
+
+          if (stats.isFile()) {
+            // Notify if changed
+            if ((stats.mtime.getTime()) !== lastChange) {
+              this._throttleEvent('change', source, stats);
+            }
+            lastChange = stats.mtime.getTime();
+          } else if (stats.isDirectory()) {
+            fs.readdir(source, (err, files) => {
+              if (err) return this.emit('error', err);
+
+              files.forEach((file) => {
+                const item = path.resolve(source, file);
+
+                // New file or directory
+                if (!RE_IGNORE.test(path.basename(item)) && !this.watchers[item]) {
+                  fs.stat(item, (err, stats) =>{
+                    if (err) return;
+
+                    this._throttleEvent('create', item, stats);
+                    this.watch(item);
+                  });
+                }
+              });
+            });
+          }
+        });
+      });
+    });
+  }
+
+  /**
+   * Stop watching a 'source' file or directory for changes
+   * @param {String} source
+   */
+  unwatch (source) {
+    const watcher = this.watchers[source];
+
+    if (watcher) {
+      delete this.watchers[source];
+      try {
+        watcher.close();
+      } catch (err) { /* ignore */}
+    }
+  }
+
+  /**
+   * Stop watching all sources for changes
+   */
+  clean () {
+    for (const source in this.watchers) {
+      this.unwatch(source);
+    }
+    for (const type in this._throttling) {
+      clearInterval(this._throttling[type]);
+      this._throttling[type] = 0;
+    }
+  }
+
+  /**
+   * Protect against mutiple event emits
+   * @param {String} type
+   * @param {Object} [props]
+   */
+  _throttleEvent (type, ...props) {
+    if (!this._throttling[type]) {
+      this.emit(type, ...props);
+      this._throttling[type] = setTimeout(() => {
+        this._throttling[type] = 0;
+      }, THROTTLE_TIMEOUT);
+    }
+  }
 }
-
-// Inherit
-util.inherits(Watcher, EventEmitter);
-
-/**
- * Watch a 'source' file or directory for changes
- * @param {String} source
- */
-Watcher.prototype.watch = function(source) {
-	var self = this;
-
-	if (!RE_IGNORE.test(path.basename(source))) {
-		fs.stat(source, function(err, stats) {
-			var lastChange;
-			if (err) {
-				self.emit('error', err);
-			} else {
-				lastChange = stats.mtime.getTime();
-				// Recursively parse items in directory
-				if (stats.isDirectory()) {
-					fs.readdir(source, function(err, files) {
-						if (err) self.emit('error', err);
-						files.forEach(function(file) {
-							self.watch(path.resolve(source, file));
-						});
-					});
-				}
-			}
-
-			// Store watcher objects
-			self.watchers[source] = fs.watch(source, function(evt, filename) {
-				if (existsSync(source)) {
-					fs.stat(source, function(err, stats) {
-						if (err) {
-							self.emit('error', err);
-						} else {
-							if (stats.isFile()) {
-								// Notify if changed
-								if ((stats.mtime.getTime()) !== lastChange) {
-									self._throttleEvent('change', source, stats);
-								}
-								lastChange = stats.mtime.getTime();
-							} else if (stats.isDirectory()) {
-								fs.readdir(source, function(err, files) {
-									if (err) {
-										self.emit('error', err);
-									} else {
-										files.forEach(function(file) {
-											var item = path.resolve(source, file);
-											// New file or directory
-											if (!RE_IGNORE.test(path.basename(item)) && !self.watchers[item]) {
-												fs.stat(item, function(err, stats) {
-													self._throttleEvent('create', item, stats);
-													self.watch(item);
-												});
-											}
-										});
-									}
-								});
-							}
-						}
-					});
-				// Deleted
-				} else {
-					self.unwatch(source);
-					self._throttleEvent('delete', source);
-				}
-			});
-		});
-	}
-};
-
-/**
- * Stop watching a 'source' file or directory for changes
- * @param {String} source
- */
-Watcher.prototype.unwatch = function(source) {
-	var watcher = this.watchers[source];
-	if (watcher) {
-		delete this.watchers[source];
-		try {
-			watcher.close();
-		} catch (err) { }
-	}
-};
-
-/**
- * Stop watching all sources for changes
- */
-Watcher.prototype.clean = function() {
-	for (var source in this.watchers) {
-		this.unwatch(source);
-	}
-	for (var type in this._throttling) {
-		clearInterval(this._throttling[type]);
-		this._throttling[type] = 0;
-	}
-};
-
-/**
- * Protect against mutiple event emits
- * @param {String} type
- * @param [props]
- */
-Watcher.prototype._throttleEvent = function(type) {
-	var self = this
-		, props = (2 <= arguments.length)
-			? Array.prototype.slice.call(arguments, 1)
-			: [];
-	if (!this._throttling[type]) {
-		this.emit.apply(this, [type].concat(props));
-		this._throttling[type] = setTimeout((function() {
-			self._throttling[type] = 0;
-		}), THROTTLE_TIMEOUT);
-	}
-};
--- test/watcher-test.js
+++ test/watcher-test.js
@@ -1,69 +1,79 @@
-var path = require('path')
-	, fs = require('fs')
-	, rimraf = require('rimraf')
-	, Watcher = require('..')
-	, watcher;
+'use strict';
 
-describe('yaw', function () {
-	before(function() {
-		process.chdir(path.resolve(__dirname, 'fixtures'));
-	});
-	beforeEach(function(done) {
-		if (fs.existsSync('test')) rimraf.sync('test');
-		if (watcher) watcher.clean();
-		fs.mkdirSync('test');
-		fs.writeFileSync('test/test.js', 'blah', 'utf8');
-		watcher = new Watcher();
-		watcher.watch(path.resolve('test'));
-		setTimeout(done, 1000);
-	});
-	after(function() {
-		rimraf.sync('test');
-	});
+const expect = require('expect.js');
+const fs = require('fs');
+const path = require('path');
+const rimraf = require('rimraf');
+const watcherFactory = require('..');
 
-	describe('watching a directory for new files', function() {
-		it('should emit a create event on addition of a new file', function (done) {
-			var newfile = path.resolve('test/dummy.js');
-			watcher.once('create', function(file, stats) {
-				file.should.eql(newfile);
-				done();
-			});
-			process.nextTick(function() {
-				fs.writeFileSync(newfile, 'blah', 'utf8');
-			});
-		});
-		it('should emit a create event on addition of a new directory', function (done) {
-			var newdir = path.resolve('test/dummy');
-			watcher.once('create', function(file, stats) {
-				file.should.eql(newdir);
-				done();
-			});
-			process.nextTick(function() {
-				fs.mkdirSync(newdir);
-			});
-		});
-	});
-	describe('watching a directory for change to an existing file', function() {
-		it('should emit a change event on update of file contents', function (done) {
-			var oldfile = path.resolve('test/test.js');
-			watcher.once('change', function(file, stats) {
-				file.should.eql(oldfile);
-				done();
-			});
-			process.nextTick(function() {
-				fs.writeFileSync(oldfile, 'blah', 'utf8');
-			});
-		});
-		it('should emit a delete event on removal from the file system', function (done) {
-			var oldfile = path.resolve('test/test.js');
-			watcher.once('delete', function(file) {
-				file.should.eql(oldfile);
-				fs.existsSync(oldfile).should.be.false;
-				done();
-			});
-			process.nextTick(function() {
-				rimraf.sync(oldfile);
-			});
-		});
-	});
+let watcher;
+
+describe('yaw', () => {
+  before(() => {
+    process.chdir(path.resolve(__dirname, 'fixtures'));
+  });
+
+  beforeEach((done) => {
+    if (fs.existsSync('test')) rimraf.sync('test');
+    if (watcher) watcher.clean();
+    fs.mkdirSync('test');
+    fs.writeFileSync('test/test.js', 'blah', 'utf8');
+    watcher = watcherFactory();
+    watcher.watch(path.resolve('test'));
+    setTimeout(done, 1000);
+  });
+  after(() => {
+    rimraf.sync('test');
+  });
+
+  describe('watching a directory for new files', () => {
+    it('should emit a create event on addition of a new file', (done) => {
+      const newfile = path.resolve('test/dummy.js');
+
+      watcher.once('create', (file, stats) => {
+        expect(file).to.eql(newfile);
+        done();
+      });
+      process.nextTick(() => {
+        fs.writeFileSync(newfile, 'blah', 'utf8');
+      });
+    });
+    it('should emit a create event on addition of a new directory', (done) => {
+      const newdir = path.resolve('test/dummy');
+
+      watcher.once('create', (file, stats) => {
+        expect(file).to.eql(newdir);
+        done();
+      });
+      process.nextTick(() => {
+        fs.mkdirSync(newdir);
+      });
+    });
+  });
+
+  describe('watching a directory for change to an existing file', () => {
+    it('should emit a change event on update of file contents', (done) => {
+      const oldfile = path.resolve('test/test.js');
+
+      watcher.once('change', (file, stats) => {
+        expect(file).to.eql(oldfile);
+        done();
+      });
+      process.nextTick(() => {
+        fs.writeFileSync(oldfile, 'blah', 'utf8');
+      });
+    });
+    it('should emit a delete event on removal from the file system', (done) => {
+      const oldfile = path.resolve('test/test.js');
+
+      watcher.once('delete', (file) => {
+        expect(file).to.eql(oldfile);
+        expect(fs.existsSync(oldfile)).to.equal(false);
+        done();
+      });
+      process.nextTick(() => {
+        rimraf.sync(oldfile);
+      });
+    });
+  });
 });
